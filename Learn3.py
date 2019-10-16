69a1fca4430a634525fbbb483189fdfb4295fe53
ForestDan <danruixuan19910622@gmail.com>
2019-10-16 10:11:54 +0900

    add commit

--- Learn3.py
+++ Learn3.py
@@ -1,21 +1,30 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# length of the list
-print("the length of this list is " + str(len(a)))
-# access the list
-print(a[0])
-print(a[1])
-print(a[2])
-# access all elements of the list
-print(a)
-# add elements to the list
-a.append(11)
-print(a)
-# add elements to the position
-a.insert(1, 13)
-print(a)
-# delete the end of the list
-a.pop()
-print(a)
-# delete the position of the list
-a.pop(1)
-print(a)
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# # length of the list
+# print("the length of this list is " + str(len(a)))
+# # access the list
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# # access all elements of the list
+# print(a)
+# # add elements to the list
+# a.append(11)
+# print(a)
+# # add elements to the position
+# a.insert(1, 13)
+# print(a)
+# # delete the end of the list
+# a.pop()
+# print(a)
+# # delete the position of the list
+# a.pop(1)
+# print(a)
+# print("--------this is the end of the list test-------")
+# tuble can not be changed
+b = (1, 2, 3, 4)
+print(b)
+# mix tuple
+c = ("a", 1, ["2.1", 2.2])
+print(c)
+c[2][0] = 2.1
+print(c)
