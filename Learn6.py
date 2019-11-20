482e8b1a7e12419339e4778b6b19a6c9f130d0dd
ForestDan <danruixuan19910622@gmail.com>
2019-11-20 21:24:45 +0900

    44

---
+++ Learn6.py
@@ -0,0 +1,20 @@
+# test dict
+"""
+d = {'Tom': 95, 'Amy': 100, 'Sara': 1000}
+print(d['Tom'])
+
+d['Tom'] = 88
+print(d['Tom'])
+
+# test in
+print('Tom' in d)
+print('Dan' in d)
+
+# test pop in dict
+d.pop('Tom')
+print('Tom' in d)
+print(d)
+"""
+# test set
+s = set([1, 1, 2, 3, 4, 4, 5])
+print(s)
